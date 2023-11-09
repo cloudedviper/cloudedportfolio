574ad3f0613a88ade38bf9393e80f0b33f157903
Cloud <121166658+cloudedviper@users.noreply.github.com>
2023-11-09 01:31:21 -0600

    js

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const hamburger = document.getElementsByClassName("hamburger")[0]
-const mobileNavs = document.getElementsByClassName("nav-links")[0]
-hamburger.addEventListener("click", () => {
-  mobileNavs.classList.toggle("active")
-})
+// Example: Toggle navigation menu for mobile devices
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+});
+
+// Add JavaScript functionality here
+
+// Example of adding functionality to a button click
+const button = document.querySelector('.site-header button');
+button.addEventListener('click', function() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+});
+
+// Example of dynamically changing styles
+const bannerContent = document.querySelector('.banner--content');
+bannerContent.addEventListener('mouseover', function() {
+  this.style.backgroundColor = '#FF0000';
+});
+
+// You can continue adding similar functionality for other elements
+// Ensure to modify the JavaScript code as per your requirements.
+
